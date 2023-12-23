823c9c4cf191aa97447a78a3ae330153ab17d728
Murzify <murzify@gmail.com>
2023-12-23 16:55:17 +0500

    desktop theme

--- composeApp/src/androidMain/kotlin/com/murzify/meetum/ui.theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/com/murzify/meetum/ui.theme/Theme.android.kt
@@ -4,10 +4,8 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -15,35 +13,10 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import com.murzify.meetum.core.ui.Pink40
-import com.murzify.meetum.core.ui.Pink80
-import com.murzify.meetum.core.ui.Purple40
-import com.murzify.meetum.core.ui.Purple80
-import com.murzify.meetum.core.ui.PurpleGrey40
-import com.murzify.meetum.core.ui.PurpleGrey80
+import com.murzify.meetum.core.ui.DarkColorScheme
+import com.murzify.meetum.core.ui.LightColorScheme
 import com.murzify.meetum.core.ui.meetumTypography
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun MeetumTheme(
--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,23 +1,27 @@
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.murzify.meetum.MR
 import com.murzify.meetum.core.common.ComponentFactory
 import com.murzify.meetum.core.data.repository.dataModule
 import com.murzify.meetum.core.database.databaseModule
 import com.murzify.meetum.core.database.driverModule
 import com.murzify.meetum.core.di.domainModule
+import com.murzify.meetum.core.ui.MeetumTheme
 import com.murzify.meetum.initSentry
 import com.murzify.meetum.root.RealRootComponent
 import com.murzify.meetum.root.RootUi
+import dev.icerock.moko.resources.compose.stringResource
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.koin.core.Koin
+import java.awt.Dimension
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalResourceApi::class)
 fun main() = application {
     val koin = createKoin()
     initSentry()
@@ -28,8 +32,14 @@ fun main() = application {
         componentContext = componentContext,
         componentFactory
     )
-    Window(onCloseRequest = ::exitApplication) {
-        MaterialTheme(colors = darkColors()) {
+
+    Window(
+        title = stringResource(MR.strings.app_name),
+        icon = painterResource("drawable/ic_launcher.webp"),
+        onCloseRequest = ::exitApplication
+    ) {
+        window.minimumSize = Dimension(800, 600)
+        MeetumTheme() {
             isSystemInDarkTheme()
             rootComponent.onCalcWindow(calculateWindowSizeClass())
             RootUi(rootComponent)
