2fe3490a9b3f802a8b4d94f700c53de58585a0ff
Murzify <murzify@gmail.com>
2024-09-21 14:05:53 +0500

    Implement convention plugin in the datastore module

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,28 +1,13 @@
+import com.murzify.meetum.buildlogic.convention.commonMainDependencies
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.com.android.library)
+    id("core")
 }
 
-kotlin {
-    androidTarget()
-    jvm("desktop")
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(projects.core.common)
-                implementation(libs.datastore.prefs)
-                implementation(libs.firebase.database)
-                implementation(libs.koin.core)
-            }
-        }
-    }
-}
+commonMainDependencies {
+    implementation(projects.core.common)
 
-android {
-    namespace = "com.murzify.meetum.core.datastore"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    implementation(libs.datastore.prefs)
+    implementation(libs.firebase.database)
 }
